8a87ba5063ddd541ba7de9e39c5b230af730ac17
Nkallepally <kallepallynavya@gmail.com>
2022-09-01 22:13:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react'
+// const names=['Relative name','Relative name','Relative name','Relative name',]
+// const App = () => {
+//   const nameList=names.map((name,i)=><li key={name.split(' ').splice(0,1).toString().toLowerCase()+"list"+(i+1)}>{name+(i+1)}</li>)
+//   console.log(nameList)
+//   return (
+//     <div>
+//  <ol>Relative List{nameList}</ol>
+//     </div>
+//   )
+// }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// export default App
 
-export default App;
+import React,{useState} from 'react'
+
+const App=()=>{
+  const[toDoitems,settoDoitems]=useState(["Relative name"])
+  const renderList=()=>{
+      return toDoitems.map((item,index)=>{
+          return <li key={item+index}>{item+index}</li>
+      })
+  }
+  const handleAddItem=()=>{
+      settoDoitems([...toDoitems,"Relative name"])
+  }
+   return(
+      <div>
+          <ul>
+          {renderList()}
+          </ul>
+          <button onClick={handleAddItem}>Add Relatives</button>
+      </div>
+   )
+  }
+
+export default App
